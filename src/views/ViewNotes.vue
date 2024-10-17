29801b7709871838b397ce24e0ff974c4d6420ff
Momojojo <codestk@gmail.com>
2024-10-17 15:27:47 +0700

    add new note

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -4,27 +4,31 @@
       <div class="field">
         <label class="label">Message</label>
         <div class="control">
-          <textarea class="textarea" placeholder="Textarea"></textarea>
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            :disabled="newNote != ''"
+            placeholder="Add New Note"
+          ></textarea>
         </div>
       </div>
 
-      <div class="field is-grouped">
+      <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link">Submit</button>
+          <button @click="addNewNote()" class="button is-link has-background-success">
+            Add New Note
+          </button>
         </div>
-        <div class="control">
+        <!-- <div class="control">
           <button class="button is-link is-light">Cancel</button>
-        </div>
+        </div> -->
       </div>
     </div>
 
-    <div v-for="i in 5" class="card md-4">
+    <div v-for="note in notes" :key="note.id" class="card md-4">
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id rerum
-          delectus, ullam quam labore itaque odio voluptate esse hic quisquam
-          facere. Nesciunt provident nihil voluptates ducimus obcaecati minus,
-          repudiandae nam!
+          {{ note.content }}
           <a href="#">@bulmaio</a>. <a href="#">#css</a>
           <a href="#">#responsive</a>
           <br />
@@ -39,4 +43,45 @@
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+/*
+  imports
+*/
+
+const newNote = ref("regergergr");
+
+/*
+  notes
+*/
+
+const notes = ref([
+  {
+    id: "id1",
+    content:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ad aspernatur vel. Voluptatem eos aliquam qui minima, adipisci tenetur sunt. Cupiditate earum molestiae similique officia enim placeat quis! Laborum, expedita. 1",
+  },
+  {
+    id: "id2",
+    content: " Lorem ipsum dolor sit amet consectetur adipisicing elit.   ",
+  },
+  {
+    id: "id3",
+    content:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit.  officia enim placeat quis! Laborum, expedita. 3 ",
+  },
+  {
+    id: "id4",
+    content: " Lorem ipsum dolor sit amet consectetur adipisicing elit.  ",
+  },
+  {
+    id: "id5",
+    content: " Lorem  5",
+  },
+]);
+
+const addNewNote = () => {
+  console.log("Hello");
+};
+</script>
